4d43c87cf8be82e53127bb2125da0e19acdc6690
Gilbert Rodriguez <jsin1947@gmail.com>
2014-06-24 23:19:40 -0500

    Added DBAdapter and WaiterInfo class

---
+++ app/src/main/java/com/jobbolster/tipcal/app/WaiterInfo.java
@@ -0,0 +1,21 @@
+package com.jobbolster.tipcal.app;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+/**
+ * Created by Gilbert Rodriguez on 6/24/2014.
+ */
+public class WaiterInfo extends ActionBarActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.waiter_info_layout);
+    }
+
+}
